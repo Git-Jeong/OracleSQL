e8d438adb1e9c141408ddaa331457837ff0933f3
JeongJin <jeongjin9427@gmail.com>
2025-01-15 10:00:30 +0900

    Update 4_WHERE.sql

--- StudyFIle/4_WHERE.sql
+++ StudyFIle/4_WHERE.sql
@@ -1,6 +1,8 @@
 
 -------------------------------------------------------------------------------
 
+-- 4. WHEHE
+
 --직업이 IT_PROG인 사람의 모든 정보를 나열
 SELECT *
 FROM EMPLOYEES
@@ -10,3 +12,17 @@ WHERE JOB_ID = 'IT_PROG';
 SELECT *
 FROM EMPLOYEES
 WHERE EMPLOYEE_ID = 105;
+
+
+-- 4.2 산술연산자 ---------------------------------------
+SELECT SALARY, SALARY+2 AS "2를 더한 값" 
+FROM EMPLOYEES;
+
+SELECT SALARY / 4
+FROM EMPLOYEES;
+
+SELECT 
+    LAST_NAME || ' ' || FIRST_NAME AS 풀네임, SALARY AS 월급
+FROM EMPLOYEES
+WHERE SALARY <= 5000
+ORDER BY SALARY;
